fc8f6c779e90e5743e8c94acc041c348fc7d84e5
Khadiza083 <khadizakhatun4083@gmail.com>
2023-05-12 04:47:46 +0600

    blog react to pdf added

--- src/Pages/Home/ChefDetails/Details.jsx
+++ src/Pages/Home/ChefDetails/Details.jsx
@@ -2,40 +2,43 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { Card, Col, Image } from 'react-bootstrap';
 import { FaThumbsUp } from 'react-icons/fa';
+import LazyLoad from 'react-lazy-load';
 import { Link, useParams } from 'react-router-dom';
-import { AuthContext } from '../../../Providers/AuthProviders';
+import { Document, Page, pdfjs } from 'react-pdf';
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 
 const Details = ({ chef }) => {
-    
+
     const [recipes, setRecipes] = useState([])
     const { chef_name, chef_image, experience, likes, chef_id } = chef
 
-    useEffect( ()=> {
+    useEffect(() => {
         fetch(`http://localhost:5000/recipes/${chef_id}`)
-        .then(res => res.json())
-        .then(data => setRecipes(data))
+            .then(res => res.json())
+            .then(data => setRecipes(data))
     })
     // console.log(chef_name);
     return (
 
         <Col xs={12} sm={3} className='text-center rounded py-2 m-4 border-1'>
-
-            <Card className='mb-4 col ' sm={3}>
-                <div className='mx-auto p-4'>
-                    <Image src={chef_image} width={200} height={250} alt="" />
-                </div>
-                <Card.Body>
-                    <Card.Title>{chef_name}</Card.Title>
-                    <div>
-                        <small className='me-4'>{experience}</small>
-                        <small className=''><FaThumbsUp size={20} className='mb-2'></FaThumbsUp> {likes}</small>
-                        <h6>No. of Recipes: {recipes.length}</h6>
+            <LazyLoad height='auto'>
+                <Card className='mb-4 col ' sm={3}>
+                    <div className='mx-auto p-4'>
+                        <Image src={chef_image} width={200} height={250} alt="" />
                     </div>
-                    <div><Link to={`/chefRecipe/${chef_id}`}> <button className='custom-bg border-0 px-4 text-white rounded py-2 my-2'>View Recipe</button>
-                    </Link>
-                    </div>
-                </Card.Body>
-            </Card>
+                    <Card.Body>
+                        <Card.Title>{chef_name}</Card.Title>
+                        <div>
+                            <small className='me-4'>{experience}</small>
+                            <small className=''><FaThumbsUp size={20} className='mb-2'></FaThumbsUp> {likes}</small>
+                            <h6>No. of Recipes: {recipes.length}</h6>
+                        </div>
+                        <div><Link to={`/chefRecipe/${chef_id}`}> <button className='custom-bg border-0 px-4 text-white rounded py-2 my-2'>View Recipe</button>
+                        </Link>
+                        </div>
+                    </Card.Body>
+                </Card>
+            </LazyLoad>
         </Col>
 
 
